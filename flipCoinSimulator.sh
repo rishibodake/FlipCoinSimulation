e8f354b6f8d5d8aa1012c3f8470b14c287e254f0
rishibodake <rishibodake@gmail.com>
2020-03-06 16:54:44 +0530

    Added:TripletCombinationFeautre

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -6,12 +6,12 @@ isHead=1
 loopRunner=0
 flips=10
 flipCoinResult=O
-#doubletResult=" "
 declare -A singletCombinations
 declare -A doubletCombinations
-
+declare -A tripletCombinations
 singletCombinations=(["H"]=0 ["T"]=0)
-doubletCombinations=(["HH"]=0 ["TT"]=0 ["TH"]=0 ["HT"]=0 )
+doubletCombinations=(["HH"]=0 ["TT"]=0 ["TH"]=0 ["HT"]=0)
+tripletCombinations=(["HHH"]=0 ["TTT"]=0 ["THT"]=0 ["HTH"]=0 ["TTH"]=0 ["HTT"]=0 ["HHT"]=0 ["THH"]=0)
 function flipCoin(){
 	if [[ $((RANDOM%2)) -eq $isHead ]]
 	then
@@ -63,6 +63,59 @@ function genrateDoubletCombinations(){
 	hTPercentage=`expr "scale=2;(${doubletCombinations[HT]}*100)/$flips" | bc -l`
 	tHPercentage=`expr "scale=2;(${doubletCombinations[TH]}*100)/$flips" | bc -l`
 }
-
-#genrateSingletCombinations
+function genrateTripletCombination(){
+	while [[ $loopRunner -lt $flips ]]
+	do
+		flipCoin
+		temp1=$flipCoinResult
+		flipCoin
+		temp2=$flipCoinResult
+		flipCoin
+		temp3=$flipCoinResult
+		if [[ $temp1$temp2$temp3 == HHH ]]
+		then
+			tripletCombinations[HHH]=$((${tripletCombinations[HHH]}+1))
+		elif [[ $temp1$temp2$temp3 == TTT ]]
+		then
+			tripletCombinations[TTT]=$((${tripletCombinations[TTT]}+1))
+		elif [[ $temp1$temp2$temp3 == THT ]]
+		then
+			tripletCombinations[THT]=$((${trepletCombinations[THT]}+1))
+		elif [[ $temp1$temp2$temp3 == HTH ]]
+		then
+			tripletCombinations[HTH]=$((${tripletCombinations[HTH]}+1))
+		elif [[ $temp1$temp2$temp3 == TTH ]]
+		then
+			tripletCombinations[TTH]=$((${tripletCombinations[TTH]}+1))
+		elif [[ $temp1$temp2$temp3 == HTT ]]
+		then
+			tripletCombinations[HTT]=$((${tripletCombinations[HTT]}+1))
+		elif [[ $temp1$temp2$temp3 == HHT ]]
+		then
+			tripletCombinations[HHT]=$((${tripletCombinations[HHT]}+1))
+		elif [[ $temp1$temp2$temp3 == THH ]]
+      then
+         tripletCombinations[THH]=$((${tripletCombinations[THH]}+1))
+		fi
+	((loopRunner++))
+done
+	HHHper=`expr "scale=2;(${tripletCombinations[HHH]}*100)/$flips" | bc -l`
+	TTTper=`expr "scale=2;(${tripletCombinations[TTT]}*100)/$flips" | bc -l`
+	THTper=`expr "scale=2;(${tripletCombinations[THT]}*100)/$flips" | bc -l`
+	HTHper=`expr "scale=2;(${tripletCombinations[HTH]}*100)/$flips" | bc -l`
+	TTHper=`expr "scale=2;(${tripletCombinations[TTH]}*100)/$flips" | bc -l`
+   HTTper=`expr "scale=2;(${tripletCombinations[HTT]}*100)/$flips" | bc -l`
+   HHTper=`expr "scale=2;(${tripletCombinations[HHT]}*100)/$flips" | bc -l`
+   THHper=`expr "scale=2;(${tripletCombinations[THH]}*100)/$flips" | bc -l`
+}
+genrateSingletCombinations
+echo ${singletCombinations[@]}
+echo ${!singletCombinations[@]}
+loopRunner=0
 genrateDoubletCombinations
+echo ${doubletCombinations[@]}
+echo ${!doubletCombinations[@]}
+loopRunner=0
+genrateTripletCombination
+echo ${tripletCombinations[@]}
+echo ${!tripletCombinations[@]}
