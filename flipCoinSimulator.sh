c413c9632a98439d80dd22322faafbe7f72800fe
rishibodake <rishibodake@gmail.com>
2020-03-06 15:44:43 +0530

    Added:DobletCombinationCalculationFeature

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,32 +3,66 @@
 #Author:Hrishikesh M Bodake
 #Since:March 6 2020 / Friday
 isHead=1
-flips=0
+loopRunner=0
+flips=10
 flipCoinResult=O
+#doubletResult=" "
 declare -A singletCombinations
+declare -A doubletCombinations
+
 singletCombinations=(["H"]=0 ["T"]=0)
+doubletCombinations=(["HH"]=0 ["TT"]=0 ["TH"]=0 ["HT"]=0 )
 function flipCoin(){
 	if [[ $((RANDOM%2)) -eq $isHead ]]
 	then
-		flipCoinResult="H"
+		flipCoinResult=H
 	else
-		flipCoinResult="T"
+		flipCoinResult=T
 	fi
 }
 function genrateSingletCombinations(){
-	while [[ $flips -lt 10 ]]
+	while [[ $loopRunner -lt $flips ]]
 	do
 		flipCoin
-		if [[	$flipCoinResult == "H" ]]
+		if [[	$flipCoinResult == H ]]
 		then
 			singletCombinations[H]=$((${singletCombinations[H]}+1))
-		elif [[ $flipCoinResult == "T" ]]
+		elif [[ $flipCoinResult == T ]]
 		then
 			singletCombinations[T]=$((${singletCombinations[T]}+1))
 		fi
-			((flips++))
+			((loopRunner++))
 	done
 	headPercentage=`expr "scale=2;(${singletCombinations[H]}*100)/$flips" | bc -l`
 	tailPercentage=`expr "scale=2;(${singletCombinations[T]}*100)/$flips" | bc -l`
 }
-genrateSingletCombinations
+function genrateDoubletCombinations(){
+	while [[ $loopRunner -lt $flips ]]
+	do
+		flipCoin
+		temp1=$flipCoinResult
+		flipCoin
+		temp2=$flipCoinResult
+		if [[ $temp1$temp2 == HH ]]
+		then
+			doubletCombinations[HH]=$((${doubletCombinations[HH]}+1))
+		elif [[ $temp1$temp2 == TT ]]
+		then
+			doubletCombinations[TT]=$((${doubletCombinations[TT]}+1))
+		elif [[ $temp1$temp2 == TH ]]
+		then
+			doubletCombinations[TH]=$((${doubletCombinations[TH]}+1))
+		elif [[ $temp1$temp2 == HT ]]
+		then
+			doubletCombinations[HT]=$((${doubletCombinations[HT]}+1))
+		fi
+		((loopRunner++))
+	done
+	hHPercentage=`expr "scale=2;(${doubletCombinations[HH]}*100)/$flips" | bc -l`
+	tTPercentage=`expr "scale=2;(${doubletCombinations[TT]}*100)/$flips" | bc -l`
+	hTPercentage=`expr "scale=2;(${doubletCombinations[HT]}*100)/$flips" | bc -l`
+	tHPercentage=`expr "scale=2;(${doubletCombinations[TH]}*100)/$flips" | bc -l`
+}
+
+#genrateSingletCombinations
+genrateDoubletCombinations
