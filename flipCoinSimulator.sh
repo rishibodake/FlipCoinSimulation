81deca62afafb16ebed4b55d6d78ae4ae22c3a94
rishibodake <rishibodake@gmail.com>
2020-03-06 14:54:27 +0530

    Added:SingletCombinationCalculationFeature

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,12 +3,32 @@
 #Author:Hrishikesh M Bodake
 #Since:March 6 2020 / Friday
 isHead=1
+flips=0
+flipCoinResult=O
+declare -A singletCombinations
+singletCombinations=(["H"]=0 ["T"]=0)
 function flipCoin(){
 	if [[ $((RANDOM%2)) -eq $isHead ]]
 	then
-		echo H
+		flipCoinResult="H"
 	else
-		echo T
+		flipCoinResult="T"
 	fi
 }
-flipCoin
+function genrateSingletCombinations(){
+	while [[ $flips -lt 10 ]]
+	do
+		flipCoin
+		if [[	$flipCoinResult == "H" ]]
+		then
+			singletCombinations[H]=$((${singletCombinations[H]}+1))
+		elif [[ $flipCoinResult == "T" ]]
+		then
+			singletCombinations[T]=$((${singletCombinations[T]}+1))
+		fi
+			((flips++))
+	done
+	headPercentage=`expr "scale=2;(${singletCombinations[H]}*100)/$flips" | bc -l`
+	tailPercentage=`expr "scale=2;(${singletCombinations[T]}*100)/$flips" | bc -l`
+}
+genrateSingletCombinations
